6e153392b65112abf9a3df0e8701f304cf8999bb
Gabryel Bele <belegabryel19@gmail.com>
2023-09-03 17:59:42 -0300

    Service completed

---
+++ src/main/java/com/example/desafio/todolist/service/ToDoService.java
@@ -0,0 +1,40 @@
+package com.example.desafio.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.desafio.todolist.model.Todo;
+import com.example.desafio.todolist.repository.ToDoRepository;
+
+@Service
+public class ToDoService {
+    private ToDoRepository toDoRepository;
+
+    public ToDoService(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+
+    }
+
+    public List<Todo> create(Todo todo) {
+        toDoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending());
+        return toDoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        toDoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        toDoRepository.deleteById(id);
+        return list();
+    }
+}
